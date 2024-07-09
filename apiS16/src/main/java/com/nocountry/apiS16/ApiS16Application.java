2688b24bee10afb39b665adaef245403db60dae1
joaquinBrassinne <joakobrassinne@gmail.com>
2024-07-09 12:10:46 -0300

    add SpringBoot dependencies

---
+++ apiS16/src/main/java/com/nocountry/apiS16/ApiS16Application.java
@@ -0,0 +1,13 @@
+package com.nocountry.apiS16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiS16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiS16Application.class, args);
+	}
+
+}
