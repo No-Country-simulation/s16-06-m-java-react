7be62c0f1947954bb97b5ac4b0fd9fa412031074
Rama <45132664+rama42111869@users.noreply.github.com>
2024-07-22 13:40:04 -0300

    Update form

--- banco-de-ortesis/src/components/ArticleForm.jsx
+++ banco-de-ortesis/src/components/ArticleForm.jsx
@@ -1,13 +1,14 @@
 // src/components/ArticleForm.jsx
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Styles.css';
-import { createArticle } from '../services/ArticleService';
-import { useNavigate } from 'react-router-dom';
+import { createArticle, updateArticle } from '../services/ArticleService';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 const ArticleForm = () => {
-  //this is a temporal funcion, will delete when backend be connected.
-
   const navigate = useNavigate();
+  const { id } = useParams();
+  const location = useLocation();
+  const currentProduct = location.state?.product || null;
 
   const formatDate = (date) => {
     const d = new Date(date);
@@ -23,13 +24,21 @@ const ArticleForm = () => {
 
   const [product, setProduct] = useState({
     name: '',
+    category: '',
+    subcategory: '',
     description: '',
     creationDate: formatDate(Date.now()),
     available: true,
     state: '',
-    category: '',
     img: ''
   });
+
+  useEffect(() => {
+    if (currentProduct) {
+      delete currentProduct.id;
+      setProduct(currentProduct);
+    }
+  }, [currentProduct]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -39,13 +48,26 @@ const ArticleForm = () => {
     }));
   };
 
-  const handleSubmit = async(e) => {
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setProduct((prevProduct) => ({
+        ...prevProduct,
+        img: reader.result
+      }));
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Lógica para subir el artículo
     try {
-      const response = await createArticle(product);
+      const response = currentProduct != null ? await updateArticle(id, product) : await createArticle(product);
       console.log(response);
-      if(response) alert('Articulo registrado exitosamente');
+      if (response) alert('Artículo registrado exitosamente');
       navigate('/');
     } catch (error) {
       console.error(error);
@@ -53,67 +75,87 @@ const ArticleForm = () => {
     console.log(product);
   };
 
+  const handleCancel = () => {
+    navigate('/profile');
+  };
+
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit} className="article-form">
-        <h2>Publicar</h2>
-        <div>
-          <label>Titulo:</label>
+      <h2 className="text-center text-2xl font-bold mb-6">Publicar</h2>
+        <div className="form-group">
+          <label htmlFor="name">Título:</label>
           <input
             type="text"
+            id="name"
             name="name"
             value={product.name}
             onChange={handleChange}
             required
           />
         </div>
-        <select
-          name="category"
-          value={product.category}
-          onChange={handleChange}
-          required
-        >
-          <option value="" disabled>Seleccione una categoría</option>
-          <option value="Miembro Superior">Miembro Superior</option>
-          <option value="Miembro Inferior">Miembro Inferior</option>
-          <option value="Axiales">Axiales</option>
-          <option value="Sillas de ruedas">Sillas de ruedas</option>
-          <option value="Bastones">Bastones</option>
-          <option value="Andadores">Andadores</option>
-          <option value="Cama ortopédica">Cama ortopédica</option>
-          <option value="Colchon antiescaras">Colchon antiescaras</option>
-          <option value="Muletas">Muletas</option>
-        </select>
-        <div>
-          <label>Descripción</label>
+        <div className="form-group">
+          <label htmlFor="category">Categoría:</label>
+          <select
+            id="category"
+            name="category"
+            value={product.category}
+            onChange={handleChange}
+            required
+          >
+            <option value="" disabled>Seleccione una categoría</option>
+            <option value="Miembro Superior">Miembro Superior</option>
+            <option value="Miembro Inferior">Miembro Inferior</option>
+            <option value="Axiales">Axiales</option>
+            <option value="Sillas de ruedas">Sillas de ruedas</option>
+            <option value="Bastones">Bastones</option>
+            <option value="Andadores">Andadores</option>
+            <option value="Cama ortopédica">Cama ortopédica</option>
+            <option value="Colchon antiescaras">Colchon antiescaras</option>
+            <option value="Muletas">Muletas</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="subcategory">Subcategoría:</label>
+          <input
+            type="text"
+            id="subcategory"
+            name="subcategory"
+            value={product.subcategory}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Descripción:</label>
           <textarea
+            id="description"
             name="description"
             value={product.description}
             onChange={handleChange}
             required
           ></textarea>
         </div>
-        <div>
-          <label>Estado</label>
+        <div className="form-group">
+          <label htmlFor="img">Agregar Imagen:</label>
           <input
-            type="text"
-            name="state"
-            value={product.state}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Url Imagen</label>
-          <input
-            type="text"
+            type="file"
+            id="img"
             name="img"
-            value={product.img}
-            onChange={handleChange}
+            accept="image/*"
+            onChange={handleImageChange}
             required
           />
         </div>
-        <button type="submit">Subir Artículo</button>
+        <div className="form-buttons">
+          <button type="button" className="cancel-button" onClick={handleCancel}>Cancelar</button>
+          <button type="submit" className="submit-button">Publicar</button>
+        </div>
+        {product.img && (
+          <div className="form-group">
+            <img src={product.img} alt="Preview" className="image-preview" />
+          </div>
+        )}
       </form>
     </div>
   );
