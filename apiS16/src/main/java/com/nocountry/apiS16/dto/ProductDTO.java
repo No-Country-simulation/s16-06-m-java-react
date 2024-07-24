37f6d8bf51d263025619b9fe9cacfc93a89f277c
Pablo <119269816+Pablo-r-stack@users.noreply.github.com>
2024-07-24 10:19:14 -0300

    Backend (#10)
    * add SpringBoot dependencies
    
    * I created APIs, crud and relations of Users and Comments
    
    * CRUD product
    
    * pruebas producto en postman del crud
    
    * CRUD Category
    
    * ajustes createProduct
    
    * pruebas postman a las entidades Product y Category CRUD
    
    * Configuracion de AuthenticationManage y su UserDetails. Creacion del exception para ObjectNotFound. Agregue el atributo contraseña a user
    
    * relaciones hechas entre productos,usuarios y comentarios
    
    * Creacion de CustomerRestController, AuthenticationService y la implementación del mismo, Creacion de JwtService, HttpSecurityConfig y el SecurityFilterChain
    
    * Elaboración del readme
    
    * Update readme.md
    
    * logo subido
    
    * Update readme.md
    
    * Update logo readme.md
    
    * Ramiro update readme.md
    
    * Agregar logo al README
    
    * Guardar cambios locales en readme.md
    
    * readme update
    
    * Agregado de favoritos!
    
    ---------
    
    Co-authored-by: joaquinBrassinne <joakobrassinne@gmail.com>
    Co-authored-by: Mariugiraldo <93294912+Mariugiraldo@users.noreply.github.com>
    Co-authored-by: maria.eugenia <mariugiraldo40@gmail.com>
    Co-authored-by: Gonzalo Bolognese <86672222+GonzaBolognese@users.noreply.github.com>
    Co-authored-by: Gonzalo Bolognese <gonza.1703@gmail.com>
    Co-authored-by: Joaquin Brassinne <146142901+joaquinBrassinne@users.noreply.github.com>

---
+++ apiS16/src/main/java/com/nocountry/apiS16/dto/ProductDTO.java
@@ -0,0 +1,26 @@
+package com.nocountry.apiS16.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+public class ProductDTO implements Serializable {
+
+    private Long idProduct;
+    private String name;
+    private Long idUser;
+    private String description;
+    private Date creationDate;
+    private boolean available;
+    private boolean state;
+    private String imageURL;
+    private Long categoryId;
+    private CategoryDTO category;
+}
