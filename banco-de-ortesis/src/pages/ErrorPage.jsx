4b0fa3a3940a9655e46c4e5a9b92205c9dc7c394
Rama <45132664+rama42111869@users.noreply.github.com>
2024-07-29 11:39:58 -0300

    Update ErrorPage.jsx

--- banco-de-ortesis/src/pages/ErrorPage.jsx
+++ banco-de-ortesis/src/pages/ErrorPage.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
+
+    const brElements = [];
+    for (let i = 0; i < 8; i++) {
+      brElements.push(<br key={i} />);
+    }
   const navigate = useNavigate();
 
   const handleGoBack = () => {
@@ -13,18 +18,21 @@ const ErrorPage = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-      <h1 className="text-6xl font-bold text-gray-800 mb-4">Error</h1>
-      <p className="text-xl text-gray-600 mb-8">Lo sentimos, la página que estás buscando no existe.</p>
+    <>
+        {brElements}
+        <div className="profile-header mb-4">
+        <div className="profile-pic"><img src="/img/logo.webp" alt="logo" />
+        </div> <h1 className="text-6xl font-bold text-gray-800 mb-4">Diagnóstico: Error 404</h1>
+      <p className="text-xl text-gray-600 mb-8">Prescripción: ¡Redirigir a la página principal!</p>
       <div className="flex space-x-4">
         <button
           onClick={handleGoHome}
-        className="bg-[#848484] hover:bg-[#6c6c6c] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        className="bg-[#1F7A8C] hover:bg-[#6c6c6c] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
-          Ir al Inicio
-        </button>
+          Volver al Inicio
+        </button></div>
       </div>
-    </div>
+    </>
   );
 };
 
